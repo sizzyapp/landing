38e8de4021a07d83405c1a1c4ecec7efad2171c0
kitze <kristijan.mkd@gmail.com>
2022-07-27 12:58:53 +0200

    update screenshots and fix layout

--- src/components/Benefits.tsx
+++ src/components/Benefits.tsx
@@ -1,14 +1,14 @@
 import { Center, Container, Stack, Title } from "@mantine/core";
 import Benefit from "components/Benefit";
-import responsive from "assets/responsive.png";
-import screenshot from "assets/screenshot.png";
-import projects from "assets/projects.png";
+import saveTestingTime from "assets/new/save-testing-time.png";
+import presentations from "assets/new/presentations.png";
+import projects from "assets/new/projects.png";
 import misc from "assets/misc.png";
 import inspectcss from "assets/inspectcss.gif";
 
 const Benefits: React.FC = () => {
   return (
-    <Container id="benefits">
+    <Container size="xl" className="sizzy-red-1" id="benefits">
       <Center>
         <Stack>
           <Title
@@ -30,7 +30,7 @@ A set of well-thought tools and features allow you to develop and test for all u
               content:
                 "Recently migrated a site from global sass to css modules and just having the current production page and the development one next to each other with click & scroll sync is seriously a killer feature within @sizzyapp. Can really recommend it!",
             }}
-            image={responsive.src}
+            image={saveTestingTime.src}
           />
           <Benefit
             title="Pretty Presentations"
@@ -40,7 +40,7 @@ A set of well-thought tools and features allow you to develop and test for all u
               content:
                 "Sizzy is a powerful selling tool.  When we have a prospect who doesn't have a responsive site, when we show them their site on Sizzy, they are impressed and we look even more professional.",
             }}
-            image={screenshot.src}
+            image={presentations.src}
           />
           <Benefit
             title="Smooth Context Switching"
