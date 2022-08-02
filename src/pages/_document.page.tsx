ffa5bee00f1e2e25ccc14085d946198a9f42f9ad
Praneet Rohida <praneetrohida@gmail.com>
2022-08-02 14:24:39 +0530

    remove emotion cache

--- src/pages/_document.page.tsx
+++ src/pages/_document.page.tsx
@@ -1,23 +1,20 @@
-import Document, { DocumentContext } from "next/document";
-import { ServerStyles, createStylesServer } from "@mantine/next";
-import { emotionCache } from "styles/emotion-cache";
+import { createGetInitialProps } from "@mantine/next";
+import Document, { Head, Html, Main, NextScript } from "next/document";
 
-const stylesServer = createStylesServer(emotionCache);
+const getInitialProps = createGetInitialProps();
 
-export default class _Document extends Document {
-  static async getInitialProps(ctx: DocumentContext) {
-    const initialProps = await Document.getInitialProps(ctx);
+export default class _documentPage extends Document {
+  static getInitialProps = getInitialProps;
 
-    return {
-      ...initialProps,
-      styles: [
-        initialProps.styles,
-        <ServerStyles
-          html={initialProps.html}
-          server={stylesServer}
-          key="styles"
-        />,
-      ],
-    };
+  render() {
+    return (
+      <Html>
+        <Head />
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    );
   }
 }
