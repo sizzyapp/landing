e7022faad3b33acf43af55b90fec53ea3fb8f44a
Fran Zekan <zekan.fran369@gmail.com>
2022-08-04 10:47:48 +0200

    Fix styles getting broken with MetaTags because of the font

--- src/pages/_document.page.tsx
+++ src/pages/_document.page.tsx
@@ -1,4 +1,12 @@
-import Document, { DocumentContext } from "next/document";
+import React from "react";
+import Document, {
+  DocumentContext,
+  Html,
+  Head,
+  Main,
+  NextScript,
+} from "next/document";
+
 import { ServerStyles, createStylesServer } from "@mantine/next";
 import { emotionCache } from "styles/emotion-cache";
 
@@ -19,5 +27,22 @@ export default class _Document extends Document {
         />,
       ],
     };
+  }
+
+  render() {
+    return (
+      <Html>
+        <Head>
+          <link
+            href="https://fonts.googleapis.com/css?family=Lato:300,400,700&display=swap"
+            rel="stylesheet"
+          />
+        </Head>
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    );
   }
 }
