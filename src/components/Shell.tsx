bc4027a8093df85001be10307a32a8f5854fb1d7
Kitze <kristijan.mkd@gmail.com>
2022-11-22 10:31:53 +0100

    add black friday banner

--- src/components/Shell.tsx
+++ src/components/Shell.tsx
@@ -1,14 +1,15 @@
-import { AppShell, AppShellProps } from "@mantine/core";
-import { FooterSimple } from "sections/Footer";
-import { ResponsiveHeader } from "sections/HeaderSection/Header";
+import { AppShell, AppShellProps, Text } from "@mantine/core";
+import { Spotlight } from "components/Spotlight";
 import Wrapper, { WrapperProps } from "components/Wrapper";
 import Conditional from "conditional-wrap";
 import { footerLinks } from "config/links";
-import { Spotlight } from "components/Spotlight";
+import dynamic from "next/dynamic";
 import React from "react";
 import { ReactGenieAnimations } from "react-genie-with-emotion";
+import { FooterSimple } from "sections/Footer";
+import { ResponsiveHeader } from "sections/HeaderSection/Header";
+import { Horizontal, Vertical } from "styles/layout-components";
 import { RealReactFC } from "types";
-import dynamic from "next/dynamic";
 
 const CookieNotice = dynamic(() => import("components/CookieNotice"), { ssr: false });
 
@@ -21,7 +22,16 @@ const Shell: RealReactFC<
 > = ({ children, wrapperProps, wrapper = true, padding }) => {
   return (
     <AppShell
-      header={<ResponsiveHeader />}
+      header={
+        <Vertical fullW>
+          <Horizontal center fullW sx={{ backgroundColor: "black", height: 30 }} color="white">
+            <Text sx={{ textAlign: "center", fontWeight: "bold" }} color="white">
+              🔥 Black Friday Sale - 50% off 🔥
+            </Text>
+          </Horizontal>
+          <ResponsiveHeader />
+        </Vertical>
+      }
       footer={<FooterSimple links={footerLinks} />}
       styles={(theme) => ({
         main: {
