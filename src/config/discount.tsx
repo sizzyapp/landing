15fbcd125cfca4a5bfbe2d7cbf40e0bc81761bed
Kitze <kristijan.mkd@gmail.com>
2023-12-12 08:29:06 +0100

    remove sale

--- src/config/discount.tsx
+++ src/config/discount.tsx
@@ -1,4 +1,4 @@
 export const discountText = <>🔥 Black Friday Sale - 50 % off 🔥</>;
 export const discountCoupon = <>BLACKFRIDAY2023 </>;
 
-export const HAS_DISCOUNT = true;
+export const HAS_DISCOUNT = false;
